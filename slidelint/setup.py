33bd3b4fc6bda04770e45e9165b7be5c8d1b2eb9
enkidulan <enkidulan@gmail.com>
2013-09-16 23:27:34 +0300

    initial commit

---
+++ slidelint/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1dev'
+
+setup(name='slidelint',
+      version=version,
+      description="Reads in PDF of presentation slides and checks common problems, outputs a summary report on the problems.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='',
+      author_email='supamaxy@gmail.com',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
