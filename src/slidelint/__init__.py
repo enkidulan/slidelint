2d74b7928972a8fc3895c496af886cba1fff0f0d
enki <enkidulan@gmail.com>
2013-09-18 19:58:42 +0300

    added logger

--- src/slidelint/__init__.py
+++ src/slidelint/__init__.py
@@ -1,1 +1,6 @@
 #
+import logging.config
+import os.path
+
+logging.config.fileConfig(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                       'logging.conf'))
