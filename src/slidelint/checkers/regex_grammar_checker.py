01dd44940fdd64e3402ab5a862e54cf1e35a8313
enki <enki@localhost.localdomain>
2013-10-11 10:45:39 +0300

    added regex based grammar checker and gender checker regex

---
+++ src/slidelint/checkers/regex_grammar_checker.py
@@ -0,0 +1,41 @@
+import re
+import os.path
+from slidelint.utils import help as help_msg_formatter
+from slidelint.pdf_utils import convert_pdf_to_text
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_file_path(path):
+    """ Files REGEX finder  """
+    path = path.strip()
+    if not os.path.sep in path:
+        path = os.path.join(here, 'regex_rules', path)
+    if os.path.isfile(path):
+        return path
+    raise ValueError("The file with REGEX rules can't be found: '%s'" % path)
+
+
+def main(target_file=None, source_file=None, re_options=None, id=None,
+         msg_name=None, msg=None, help=None, msg_info=None):
+    """ Runner fro regexp config files. Takes rules_source file can """
+    pattern = open(get_file_path(source_file), 'rb').read()
+    options = [getattr(re, o) for o in re_options.split('\n') if o]
+    regexp = re.compile(pattern, *options)
+    if msg_info:
+        return help_msg_formatter(
+                    (dict(id=id, msg_name=msg_name, msg=msg, help=help),),
+                    msg_info)
+    pages = convert_pdf_to_text(target_file)
+    rez = []
+    for num, page in enumerate(pages):
+        for paragraph in page:
+                match = regexp.search(paragraph)
+                if match:
+                    rez.append({
+                        'id': id,
+                        'page': '%s' % (num + 1),
+                        'msg_name': msg_name,
+                        'msg': '%s: "%s" mentioned in "%s"' % (msg, str(match.group()), paragraph),
+                        'help': help})
+    return rez
---
+++ src/slidelint/tests/TestCheckerRegexpRunner.py
@@ -0,0 +1,64 @@
+import os.path
+import unittest
+from testfixtures import compare
+
+from slidelint.checkers import regex_grammar_checker
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+
+class TestRegexGrammarChecker(unittest.TestCase):
+
+    def test_regex_grammar_checker(self):
+        target_file = os.path.join(here, 'files', 'pdfs', 'gender.pdf')
+        kwargs = {
+          'target_file': target_file,
+          'source_file': 'gendered_pronouns',
+          're_options': 'IGNORECASE',
+          'id': 'W2000',
+          'msg_name': 'gender-mention',
+          'msg': 'Gender Mention',
+          'help': 'Gendered pronouns are those that indicate gender: he, she, him, her, hers, his, himself and herself. All others, like "it, "one," and "they," are gender neutral.'}
+        rez = regex_grammar_checker.main(**kwargs)
+        compare(rez,
+                [{u'help': u'Gendered pronouns are those that indicate gender: he, she, him, her, hers, his, himself and herself. All others, like "it, "one," and "they," are gender neutral.',
+                  u'id': u'W2000',
+                  u'msg': u'Gender Mention: " guys!" mentioned in "Hello guys!"',
+                  u'msg_name': u'gender-mention',
+                  'page': '1'},
+                {u'help': u'Gendered pronouns are those that indicate gender: he, she, him, her, hers, his, himself and herself. All others, like "it, "one," and "they," are gender neutral.',
+                  u'id': u'W2000',
+                  u'msg': u'Gender Mention: "He " mentioned in "He wrote awesome code!"',
+                  u'msg_name': u'gender-mention',
+                  'page': '3'},])
+
+    def test_checker_helpers(self):
+        kwargs = {
+          'msg_info': 'All',
+          'source_file': 'gendered_pronouns',
+          're_options': 'IGNORECASE',
+          'id': 'W2000',
+          'msg_name': 'gender-mention',
+          'msg': 'Gender Mention',
+          'help': 'Gendered pronouns are those that indicate gender: he, she, him, her, hers, his, himself and herself. All others, like "it, "one," and "they," are gender neutral.'}
+        help_msgs = regex_grammar_checker.main(**kwargs)
+        compare(help_msgs,
+                [{'help': 'Gendered pronouns are those that indicate gender: he, she, him, her, hers, his, himself and herself. All others, like "it, "one," and "they," are gender neutral.',
+                  'id': 'W2000',
+                  'msg': 'Gendered pronouns are those that indicate gender: he, she, him, her, hers, his, himself and herself. All others, like "it, "one," and "they," are gender neutral.',
+                  'msg_name': 'gender-mention',
+                  'page': ''},])
+        kwargs['msg_info'] = ['W2000']
+        help_msgs = regex_grammar_checker.main(**kwargs)
+        compare(help_msgs,
+                [{u'help': u'Gendered pronouns are those that indicate gender: he, she, him, her, hers, his, himself and herself. All others, like "it, "one," and "they," are gender neutral.',
+                  u'id': u'W2000',
+                  u'msg': u'Gendered pronouns are those that indicate gender: he, she, him, her, hers, his, himself and herself. All others, like "it, "one," and "they," are gender neutral.',
+                  u'msg_name': u'gender-mention',
+                  'page': ''},])
+        kwargs['msg_info'] = ['W8001']
+        compare(regex_grammar_checker.main(**kwargs),
+                [])
+
+if __name__ == '__main__':
+    unittest.main()
